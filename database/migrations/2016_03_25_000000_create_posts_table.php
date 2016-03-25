7339bca16f272c204ee00fd01d8be0059428873e
bhaway <bhaway.mca@gmail.com>
2016-03-25 17:55:45 +0530

    posts table added

---
+++ database/migrations/2016_03_25_000000_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('media1_url', 255);
+            $table->string('media2_url', 255);
+            $table->string('media3_url', 255);
+			$table->string('media4_url', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
