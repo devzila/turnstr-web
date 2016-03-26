2141bdaede1a0a8991e58c0e0ea2e123259f8a61
bhaway <bhaway.mca@gmail.com>
2016-03-26 12:32:18 +0530

    comments api added

---
+++ app/Http/Controllers/Api/CommentsController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use Response;
+use App\Models\Comments;
+use App\Models\DeviceSession;
+
+class CommentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()  
+    {
+         $comments = Comments::where('user_id', DeviceSession::get()->user->id)->get();
+		 return  Response::json($comments, 200);
+		 
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+      $result = Comments::create([
+        'user_id' => DeviceSession::get()->user->id,
+         'post_id' => $request->input('post_id'),
+		 'comments' => $request->input('comments'),
+	 ]);
+        return Response::json($result, 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+		$comments = Comments::find($id);
+		return $comments->toJson();
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+		$comments = Comments::find($id);
+    	$comments->comments = $request->input('comments');
+		$comments->update();
+        return $comments->toJson();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+		$comments = Comments::find($id);
+		$comments->delete();
+        return Response::json(['status'=>"OK"], 200);
+    }
+}
