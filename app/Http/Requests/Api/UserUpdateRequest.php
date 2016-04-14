8c9da863a91912afba3bf4880cde68af1a4df486
bhupindergarg <vikisingla1992@gmail.com>
2016-04-14 22:48:48 +0530

    error removing from request file

---
+++ app/Http/Requests/Api/UserUpdateRequest.php
@@ -0,0 +1,49 @@
+<?php namespace App\Http\Requests\Api;
+
+use App\Http\Requests\Request;
+use App\Models\Api;
+use App\Helpers\ResponseClass;
+
+class UserUpdateRequest extends Request {
+
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		return true;
+	}
+
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+		return [
+			     'name'     => 'sometimes',
+            'email'         => 'required|email',
+            'username'      => 'required|min:4',
+            'phone_number'  => 'sometimes|regex:/^\d{10,11}$/'         
+		];
+	}
+
+    /**
+     * Get the proper failed validation response for the request.
+     *
+     * @param  array  $errors
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function response(array $errors)
+    {
+      $finalErrors = array();
+        foreach ($errors as $key => $value) {
+          $finalErrors[$key] = $value[0];
+        }
+        return ResponseClass::Prepare_Response(['message'=> "There were errors in the input sent. Please check your request and try again",'action' => 'Check for errors in the data sent','errors'=>$finalErrors],false,422);
+    }
+
+}
