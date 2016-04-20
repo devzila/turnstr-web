f729114515214720f2d1ce0a21494457142439c4
bhupindergarg <vikisingla1992@gmail.com>
2016-04-20 08:59:54 +0530

    activity list method change

--- app/Http/routes.api.php
+++ app/Http/routes.api.php
@@ -68,7 +68,7 @@ Route::group(['middleware' => ['turnstr.api'], 'prefix' => 'api'], function () {
             'as' => 'likePost',
             'middleware' => []
         ]);
-        Route::post('activityList', [
+        Route::get('activityList', [
             'uses' => 'ActivityController@getTenActivity',
             'as' => 'activityList',
             'middleware' => []
