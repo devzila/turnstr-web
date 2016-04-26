f773225cfcd07b3c79cb5143e1a616d8eb0ea171
bhupindergarg <vikisingla1992@gmail.com>
2016-04-26 23:49:05 +0530

    post api changes

--- app/Http/Controllers/Api/PostsController.php
+++ app/Http/Controllers/Api/PostsController.php
@@ -161,13 +161,13 @@ class PostsController extends Controller
         $userId = DeviceSession::get()->user->id;
         $posts = Posts::followPosts($userId);
 
-        foreach ($posts as $key => $value) {
-            dd($posts[$key]->media1_type);
-            $posts[$key]->media1_type = end(explode('.',$value->media1_url));
-            $posts[$key]->media2_type = end(explode('.',$value->media2_url));
-            $posts[$key]->media3_type = end(explode('.',$value->media3_url));
-            $posts[$key]->media4_type = end(explode('.',$value->media4_url));
-        }
+        // foreach ($posts as $key => $value) {
+        //     dd($posts[0]->media1_type);
+        //     $posts[$key]->media1_type = end(explode('.',$value->media1_url));
+        //     $posts[$key]->media2_type = end(explode('.',$value->media2_url));
+        //     $posts[$key]->media3_type = end(explode('.',$value->media3_url));
+        //     $posts[$key]->media4_type = end(explode('.',$value->media4_url));
+        // }
 
         return ResponseClass::Prepare_Response($posts,'List of posts',true,200);
     }
