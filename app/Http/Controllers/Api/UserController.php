d2bf33953868fb284e2cd4ae345c48b964036dfc
Nilay <nilayanand@gmail.com>
2016-04-26 16:54:54 +0530

    Fix missing image class1

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -24,7 +24,7 @@ use URL;
 use Input;
 use Validator;
 use File;
-use Intervention\Image\Facades\Image as Image;
+use Img;
 class UserController extends Controller {
     /**
      * The Http Request Object
@@ -212,7 +212,7 @@ class UserController extends Controller {
             $fileName = Uuid::uuid1()->toString() . '.' . $extension;
             $request->file("profile_image")->move($destinationPath, $fileName);
 
-            Image::make($destinationPath.'/'.$fileName)->resize(200, 200)->rotate(90)->save($destinationPath.'/thumb/thumb_'.$fileName);
+            Img::make($destinationPath.'/'.$fileName)->resize(200, 200)->rotate(90)->save($destinationPath.'/thumb/thumb_'.$fileName);
 
            User::where('id',$userId)->update(['profile_image'=>$fileName]);
 
