39c77678b18415b8c08cb7470e6eeb4db03cc8a2
bhopu@1992 <bhupindergarg92@gmail.com>
2016-05-10 23:52:53 +0530

    commiting changes in shareURl,store api

--- app/Helpers/UniversalClass.php
+++ app/Helpers/UniversalClass.php
@@ -1,13 +1,12 @@
 <?php
 namespace App\Helpers;
 use Universal;
-
+use URL;
 class UniversalClass{
-
 
    public static function encrypt($id)
     {
-        $key = "123456";
+        $key = "123xyz";
         $id = base_convert($id, 10, 36); // Save some space
         $data = mcrypt_encrypt(MCRYPT_BLOWFISH, $key, $id, 'ecb');
         $data = bin2hex($data);
@@ -17,13 +16,23 @@ class UniversalClass{
 
     public static function decrypt($encrypted_id)
     {
-        $key = "123456";
+         $key = "123xyz";
         $data = pack('H*', $encrypted_id); // Translate back to binary
         $data = mcrypt_decrypt(MCRYPT_BLOWFISH, $key, $data, 'ecb');
         $data = base_convert($data, 36, 10);
 
         return $data;
     }
-
+    /*create Share URL for post
+        param @id
+     */
+    public static function shareUrl($id){
+        
+        $postId = self::encrypt($id);
+        
+        $postUrl = URL::to('/').'/posts/'.$postId;
+        
+        return $postUrl;
+    }
 }
 ?>
