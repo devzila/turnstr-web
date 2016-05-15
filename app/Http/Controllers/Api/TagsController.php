f980fb73fb7b9caac35fd954d801cb5d2ab2f22f
Nilay <nilayanand@gmail.com>
2016-05-16 00:17:01 +0530

    Add pagination in tagged post list

--- app/Http/Controllers/Api/TagsController.php
+++ app/Http/Controllers/Api/TagsController.php
@@ -13,19 +13,23 @@ use DB;
 
 class TagsController extends Controller
 {
+
+    const POSTS_PER_PAGE = 10;
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Request $request, $id)
     {
+        $page = $request->input('page', 0);
 
         $posts = DB::table('posts')
             ->join('post_tags', 'posts.id', '=', 'post_tags.post_id')
             ->join('users', 'posts.user_id', '=', 'users.id')
             ->where('post_tags.tag_name', '=', $id)
             ->select('posts.*', 'users.name', 'users.profile_image', 'users.username')
+            ->skip($page * self::POSTS_PER_PAGE)->take(self::POSTS_PER_PAGE)
             ->get();
 
         // add additional field
