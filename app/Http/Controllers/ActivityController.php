e70ca5e1018117fafeba2cb85c5dec95f2730166
vineet-devzila <vineet@devzila.com>
2016-06-08 15:41:38 +0530

    activity changes

---
+++ app/Http/Controllers/ActivityController.php
@@ -0,0 +1,84 @@
+<?php namespace App\Http\Controllers;
+
+use Config;
+use App\Models\Useractivity;
+use App\Models\User;
+use App\Models\Posts;
+use App\Models\Comments;
+use App\Helpers\UniversalClass;
+use Auth;
+
+ /**
+ * User Activity Class Web
+ *
+ * 
+ */
+
+class ActivityController extends Controller {
+	
+	   public function getActivity() {
+
+        $user_id = Auth::user()->id;
+        $activityList = array();
+
+        $alreadyLiked = Useractivity::getLastTenActivity($user_id);
+		
+        foreach ($alreadyLiked as $key => $value) {
+            if ($alreadyLiked[$key]->activity=='liked') {
+
+                // Removing following details 
+                unset($alreadyLiked[$key]->following_name);
+                unset($alreadyLiked[$key]->follower_name);
+                unset($alreadyLiked[$key]->follower_id);
+                unset($alreadyLiked[$key]->follower_image);
+
+                // fetching user and posts data
+                $userInfo = User::find($alreadyLiked[$key]->likedby_id);
+                $postInfo = Posts::find($alreadyLiked[$key]->turn_id);
+
+                // adding user info
+                if (count($userInfo)) {
+                    $postCount = Posts::where('user_id',$userInfo->id)->count();
+                    $userInfo->post_count = ($postCount>0) ? (string)$postCount : "0" ;
+                    $followingDetails = Useractivity::getFollowDetailByUserId($userInfo->id,$user_id);
+                    $userInfo->is_following = (count($followingDetails) && isset($followingDetails->status)) ? (int)($followingDetails->status) : 0 ;
+                    $userInfo->id = (string)($userInfo->id);
+                    $userInfoFinal =  $userInfo;
+                } else {
+                    $userInfoFinal =  '';
+                }
+                // adding post info
+                if (count($postInfo)) {
+                    $commentsCount = comments::commentsCountByPostId($alreadyLiked[$key]->turn_id);
+                    $postInfo->post_comments_count = ($commentsCount>0) ? (string)$commentsCount : "0" ;
+                    $postInfoFinal =  $postInfo;
+                    $postInfo->id =  (string)($postInfo->id);
+                    // adding share url
+                    $postInfo->shareUrl = UniversalClass::shareUrl($alreadyLiked[$key]->turn_id);
+                } else {
+                    $postInfoFinal =  '';
+                }                
+                $value->user_info = $userInfoFinal;
+                $value->post_info = $postInfoFinal;
+               
+            } else {
+                
+                unset($alreadyLiked[$key]->likedby_name);
+                unset($alreadyLiked[$key]->likedby_id);
+                unset($alreadyLiked[$key]->likedby_image);
+                unset($alreadyLiked[$key]->likedof_name);
+               
+                $userInfo = User::find($alreadyLiked[$key]->follower_id);
+                
+                $value->user_info = (count($userInfo)) ? $userInfo : '' ;
+                $value->is_following = $alreadyLiked[$key]->status;
+            }
+        }
+		return view("activity.activity",['activities'=>$alreadyLiked]);
+    }
+
+	
+}
+
+
+
---
+++ resources/views/activity/activity.blade.php
@@ -0,0 +1,36 @@
+
+@extends('layouts.app')
+@section('content')
+
+<div class="w-section content">
+        <div class="w-container activity-container">
+			@if(!count($activities))
+				<div class="w-clearfix activity-list-item">
+					<div class="userthumb"></div>
+					<div class="activity-text">
+					  <div><span class="activity-username">No Activity Found</span>
+					  </div>
+					</div>
+				 </div>
+			@else
+				@foreach($activities as $activity)
+					  <div class="w-clearfix activity-list-item">
+					  
+						<div class="userthumb">@if($activity->user_info->profile_thumb_image)<img src="{{$activity->user_info->profile_thumb_image}}">@endif</div>
+					  
+					  <div class="activity-text">
+						  <div><span class="activity-username">{{$activity->user_info->username}}</span>
+						  @if($activity->activity == 'liked')
+								&nbsp;has liked your Turn.
+						  @else
+								&nbsp; has started following you. 
+						  @endif
+						  <span class="activity-time">12w</span>
+						  </div>
+						</div>
+					  </div>
+				@endforeach
+			@endif          
+        </div>
+      </div>
+  @endsection 
