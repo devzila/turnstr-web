7e21ed7ff6bb0b7ffb318f0a92618a518244e981
vineet-devzila <vineet@devzila.com>
2016-06-15 17:58:03 +0530

    share page 404 file

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+@section('content')
+
+        <div class="container">
+            <div class="content">
+                <div class="title"><h2>Sorry, this Page isn't Available .</h2>
+				</div>
+				The link you followed may be broken or the page may have been remove. Go back to <a href="/">Turnstr</a>.
+            </div>
+        </div>
+		
+@endsection 
