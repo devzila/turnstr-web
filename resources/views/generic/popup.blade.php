42c66a3fffbbf8b5d56a6451698fc06e72d6288e
vineet-devzila <vineet@devzila.com>
2016-07-08 18:27:59 +0530

    new-file

---
+++ resources/views/generic/popup.blade.php
@@ -0,0 +1,29 @@
+<div id="confirmModal" class="modal small fade in">
+    <div class="w-container modal-contentn">
+      <div class="modal-window modal-inapp" >
+        <h1 class="share-modal-heading panel-title">Please Confirm!</h1>
+        <div class="share-platforms modal-body">
+			<div id="cmsg">Do you want to Delete this ? </div>
+        </div>
+        <a class="w-inline-block close-modal" data-dismiss="modal" id="deleteConfirm" href="javascript:;">
+          <div>Delete</div>
+        </a>
+		<a class="w-inline-block close-modal" data-dismiss="modal" href="javascript:;">
+          <div>Close</div>
+        </a>
+      </div>
+    </div>
+  </div>
+  <div id="cAlert" class="modal small fade in">
+    <div class="w-container modal-contentn">
+      <div class="modal-window modal-inapp" >
+        <h1 class="share-modal-heading panel-title">Alert!</h1>
+        <div class="share-platforms modal-body">
+			<div class="cAlertMessage"> </div>
+        </div>
+        <a class="w-inline-block close-modal" data-dismiss="modal" href="javascript:;">
+          <div>Close</div>
+        </a>
+      </div>
+    </div>
+  </div>
